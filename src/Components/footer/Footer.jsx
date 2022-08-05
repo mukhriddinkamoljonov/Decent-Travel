cc6c3323b0aa954aec1bab93f3640bd937f8728d
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-08-05 15:52:50 +0500

    fsd

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { Logo } from "../../assets/icon";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer-container">
+        {/* // You need to write the footer part of the website */}
+        <div className="footer-content">
+          <div className="">
+            <div className="footer-content-log">
+              <Logo />
+            </div>
+            <p>
+              With travala you can experience new travel and the best tourist
+              destinations that we have to offer
+            </p>
+          </div>
+          <div className="">
+            <h3 className="footer-content-head-text">Services</h3>
+            <div>
+              <ul className="footer-content-list">
+                <li>Home</li>
+                <li>Shaharlar</li>
+                <li>Diqqatga sazovor joylar</li>
+                <li>Galereya</li>
+              </ul>
+            </div>
+          </div>{" "}
+          <div className="">
+            <h3 className="footer-content-head-text">Services</h3>
+            <div>
+              <ul className="footer-content-list">
+                <li>About</li>
+                <li>Biz haqimizda</li>
+                <li>Kontaktlariimiz</li>
+                <li>F.A.Q</li>
+              </ul>
+            </div>
+          </div>{" "}
+          <div className="">
+            <h3 className="footer-content-head-text">Follow Us</h3>
+            <div>
+              <ul className="footer-content-list">
+                <li>Home</li>
+                <li>Shaharlar</li>
+                <li>Diqqatga sazovor joylar</li>
+                <li>Galereya</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
