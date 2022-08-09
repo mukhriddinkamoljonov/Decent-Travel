31eefbb834dd34a36f1e2901eb3384f557745687
Muhriddin Kamoljonov <84762506+mukhriddinkamoljonov@users.noreply.github.com>
2022-08-09 12:29:54 +0500

    Update Navbar.jsx

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -39,13 +39,13 @@ function classNames(...classes) {
 function Navbar() {
   const { t } = useTranslation();
   const navigation = [
-    { name: `${t("main")}`, href: "#", current: false },
-    { name: `${t("citys")}`, href: "#", current: false },
-    { name: `${t("pilgrimage")}`, href: "#", current: false },
-    { name: `${t("attractions")}`, href: "#", current: false },
+    { name: `${t("main")}`, href: "/", current: false },
+    { name: `${t("citys")}`, href: "/cites", current: false },
+    { name: `${t("pilgrimage")}`, href: "/umrah", current: false },
+    { name: `${t("attractions")}`, href: "/attractions", current: false },
     { name: `${t("gallery")}`, href: "#", current: false },
-    { name: `${t("about_Us")}`, href: "#", current: false },
-    { name: `${t("сommunication")}`, href: "#", current: false },
+    { name: `${t("about_Us")}`, href: "/aboutus", current: false },
+    { name: `${t("сommunication")}`, href: "/contacts", current: false },
   ];
 
   return (
