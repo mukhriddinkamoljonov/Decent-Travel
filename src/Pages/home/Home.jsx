a4a3123dcbdaf032a9a3a009ca4d0b582bff4316
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-10-12 22:07:26 +0500

    fds

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -69,268 +69,268 @@ function Home() {
         </div>
         <FullPageSections>
           <FullpageSection style={{ SectionStyle }}> */}
-            <div className="header_section-one">
-              <div className="header-bc">
-                <div className="header-content">
-                  <p className="first-text">{t("the_best")}</p>
-                  <h1 className="main-text">{t("best_travel")}</h1>
-                  <p className="last-text">
-                    {t("the_decent")}
-                    <br />
-                    {t("")}
-                  </p>
-                  <button> {t("turist_city")}</button>
-                </div>
-              </div>
-            </div>
-          {/* </FullpageSection>
+      <div className="header_section-one">
+        <div className="header-bc">
+          <div className="header-content">
+            <p className="first-text">{t("the_best")}</p>
+            <h1 className="main-text">{t("best_travel")}</h1>
+            <p className="last-text">
+              {t("the_decent")}
+              <br />
+              {t("")}
+            </p>
+            <button> {t("turist_city")}</button>
+          </div>
+        </div>
+      </div>
+      {/* </FullpageSection>
           <FullpageSection style={{ SectionStyle }} className="example_Section"> */}
-            <div className="header_section-two">
-              <div className="home_cards">
-                <h1 className="center-text">{t("turist_city")}</h1>
-                <div className="center-text-line" />
-                <div className="card_content grid gap-1">
-                  <div className="card-item">
-                    <div className="card-image">
-                      <img src={samarqand} alt="" />
-                    </div>
-                    {/* link */}
-                    <Link to="/detailed">
-                      <h1 className="card-title">Samarqand</h1>
-                    </Link>
-                  </div>{" "}
-                  <div className="card-item">
-                    <div className="card-image">
-                      <img src={xiva} alt="" />
-                    </div>
-                    <Link to="/detailed">
-                      <h1 className="card-title">Farg'ona</h1>
-                    </Link>
-                  </div>{" "}
-                  <div className="card-item">
-                    <div className="card-image">
-                      <img src={xiva} alt="" />
-                    </div>
-                    <Link to="/detailed">
-                      <h1 className="card-title">Buxoro</h1>
-                    </Link>
-                  </div>{" "}
-                  <div className="card-item">
-                    <div className="card-image">
-                      <img src={xiva} alt="" />
-                    </div>
-                    <Link to="/detailed">
-                      <h1 className="card-title">Xorazm</h1>
-                    </Link>
-                  </div>{" "}
-                  <div className="card-item">
-                    <div className="card-image">
-                      <img src={xiva} alt="" />
-                    </div>
-                    <Link to="/detailed">
-                      <h1 className="card-title">Samarqand</h1>
-                    </Link>
-                  </div>{" "}
-                  <div className="card-item">
-                    <div className="card-image">
-                      <img src={namangan} alt="" />
-                    </div>
-                    <Link to="/detailed">
-                      <h1 className="card-title">Toshkent</h1>
-                    </Link>
-                  </div>{" "}
-                  <div className="card-item">
-                    <div className="card-image">
-                      <img src={fargona} alt="" />
-                    </div>{" "}
-                    <Link to="/detailed">
-                      <h1 className="card-title">Surhandaryo</h1>
-                    </Link>
-                  </div>{" "}
-                  <div className="card-item">
-                    <div className="card-image">
-                      <img src={samarqand} alt="" />
-                    </div>{" "}
-                    <Link to="/detailed">
-                      <h1 className="card-title">Samarqand</h1>
-                    </Link>
-                  </div>
-                </div>
+      <div className="header_section-two">
+        <div className="home_cards">
+          <h1 className="center-text">{t("turist_city")}</h1>
+          <div className="center-text-line" />
+          <div className="card_content grid gap-1">
+            <div className="card-item">
+              <div className="card-image">
+                <img src={samarqand} alt="" />
               </div>
+              {/* link */}
+              <Link to="/detailed">
+                <h1 className="card-title">Samarqand</h1>
+              </Link>
+            </div>{" "}
+            <div className="card-item">
+              <div className="card-image">
+                <img src={xiva} alt="" />
+              </div>
+              <Link to="/detailed">
+                <h1 className="card-title">Farg'ona</h1>
+              </Link>
+            </div>{" "}
+            <div className="card-item">
+              <div className="card-image">
+                <img src={xiva} alt="" />
+              </div>
+              <Link to="/detailed">
+                <h1 className="card-title">Buxoro</h1>
+              </Link>
+            </div>{" "}
+            <div className="card-item">
+              <div className="card-image">
+                <img src={xiva} alt="" />
+              </div>
+              <Link to="/detailed">
+                <h1 className="card-title">Xorazm</h1>
+              </Link>
+            </div>{" "}
+            <div className="card-item">
+              <div className="card-image">
+                <img src={xiva} alt="" />
+              </div>
+              <Link to="/detailed">
+                <h1 className="card-title">Samarqand</h1>
+              </Link>
+            </div>{" "}
+            <div className="card-item">
+              <div className="card-image">
+                <img src={namangan} alt="" />
+              </div>
+              <Link to="/detailed">
+                <h1 className="card-title">Toshkent</h1>
+              </Link>
+            </div>{" "}
+            <div className="card-item">
+              <div className="card-image">
+                <img src={fargona} alt="" />
+              </div>{" "}
+              <Link to="/detailed">
+                <h1 className="card-title">Surhandaryo</h1>
+              </Link>
+            </div>{" "}
+            <div className="card-item">
+              <div className="card-image">
+                <img src={samarqand} alt="" />
+              </div>{" "}
+              <Link to="/detailed">
+                <h1 className="card-title">Samarqand</h1>
+              </Link>
             </div>
-          {/* </FullpageSection>
+          </div>
+        </div>
+      </div>
+      {/* </FullpageSection>
           <FullpageSection style={{ SectionStyle }}> */}
-            <div className="home_urmrah">
-              <div className="home_umrah_content">
-                <div className="home_umrah_image">
-                  <img src={umrah} alt="" />
-                </div>
-                <div className="home_urmrah_content_text">
-                  <h1 className="home_urmrah_title">{t("umrah_name")}</h1>
-                  <p className="home_urmrah_text">{t("umrah_text")}</p>
-                  <button className="home_urmrah_button">
-                    {" "}
-                    <Link to="/umrah">{t("see_all")}</Link>
-                  </button>
-                </div>
-              </div>
-            </div>
-          {/* </FullpageSection>
+      <div className="home_urmrah">
+        <div className="home_umrah_content">
+          <div className="home_umrah_image">
+            <img src={umrah} alt="" />
+          </div>
+          <div className="home_urmrah_content_text">
+            <h1 className="home_urmrah_title">{t("umrah_name")}</h1>
+            <p className="home_urmrah_text">{t("umrah_text")}</p>
+            <button className="home_urmrah_button">
+              {" "}
+              <Link to="/umrah">{t("see_all")}</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* </FullpageSection>
           <FullpageSection style={{ SectionStyle }}> */}
-            <div className="home_carousel">
-              <h1 className="center-text">{t("attractions")}</h1>
-              <hr className="carousel-text-line" />
-              <div className="home_carousel_content">
-                <Swiper
-                  slidesPerView={1}
-                  spaceBetween={30}
-                  slidesPerGroup={1}
-                  loop={true}
-                  loopFillGroupWithBlank={true}
-                  pagination={{
-                    clickable: true,
-                  }}
-                  navigation={true}
-                  modules={[Pagination, Navigation]}
-                  className="mySwiper"
-                >
-                  <div className="carousel-content">
-                    <SwiperSlide>
-                      <div className="carousel-item">
-                        <div className="carousel_image">
-                          <img src={karimov} alt="" />
-                        </div>
-                        <h1>{t("karimov")} </h1>
-                      </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                      <div className="carousel-item">
-                        <div className="carousel_image">
-                          <img src={alisher} alt="" />
-                        </div>
-                        <h1>{t("navoiy")} </h1>
-                      </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                      <div className="carousel-item">
-                        <div className="carousel_image">
-                          <img src={ziyorat} alt="" />
-                        </div>
-                        <h1>{t("hazrati_d")} </h1>
-                      </div>
-                    </SwiperSlide>{" "}
-                    <SwiperSlide>
-                      <div className="carousel-item">
-                        <div className="carousel_image">
-                          <img src={alisher} alt="" />
-                        </div>
-                        <h1>{t("navoiy")} </h1>
-                      </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                      <div className="carousel-item">
-                        <div className="carousel_image">
-                          <img src={karimov} alt="" />
-                        </div>
-                        <h1>{t("navoiy")} </h1>
-                      </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                      <div className="carousel-item">
-                        <div className="carousel_image">
-                          <img src={ziyorat} alt="" />{" "}
-                        </div>
-                        <h1>{t("hazrati_d")} </h1>
-                      </div>
-                    </SwiperSlide>
+      <div className="home_carousel">
+        <h1 className="center-text">{t("attractions")}</h1>
+        <hr className="carousel-text-line" />
+        <div className="home_carousel_content">
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            slidesPerGroup={1}
+            loop={true}
+            loopFillGroupWithBlank={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+          >
+            <div className="carousel-content">
+              <SwiperSlide>
+                <div className="carousel-item">
+                  <div className="carousel_image">
+                    <img src={karimov} alt="" />
                   </div>
-                </Swiper>
-              </div>
+                  <h1>{t("karimov")} </h1>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="carousel-item">
+                  <div className="carousel_image">
+                    <img src={alisher} alt="" />
+                  </div>
+                  <h1>{t("navoiy")} </h1>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="carousel-item">
+                  <div className="carousel_image">
+                    <img src={ziyorat} alt="" />
+                  </div>
+                  <h1>{t("hazrati_d")} </h1>
+                </div>
+              </SwiperSlide>{" "}
+              <SwiperSlide>
+                <div className="carousel-item">
+                  <div className="carousel_image">
+                    <img src={alisher} alt="" />
+                  </div>
+                  <h1>{t("navoiy")} </h1>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="carousel-item">
+                  <div className="carousel_image">
+                    <img src={karimov} alt="" />
+                  </div>
+                  <h1>{t("navoiy")} </h1>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="carousel-item">
+                  <div className="carousel_image">
+                    <img src={ziyorat} alt="" />{" "}
+                  </div>
+                  <h1>{t("hazrati_d")} </h1>
+                </div>
+              </SwiperSlide>
             </div>
-          {/* </FullpageSection>
+          </Swiper>
+        </div>
+      </div>
+      {/* </FullpageSection>
           <FullpageSection style={{ SectionStyle }}> */}
-            <div className="home_galereya">
-              <div className="home_galereya_content">
-                <h1 className="center-text_3">{t("gallery")} </h1>
-                <hr className="center-text-line_3" />
-                <div className="home_galereya_images">
-                  <div>
-                    <img src={saroy} alt="" />
-                    <img src={chorvoq} alt="" />
-                  </div>
-                  <div>
-                    <img src={haykal} alt="" />
-                  </div>
-                  <div>
-                    <img src={samar} alt="" />
-                    <img src={naman} alt="" />
-                  </div>
-                </div>
-                <button>{t("see_all")}</button>
-              </div>
+      <div className="home_galereya">
+        <div className="home_galereya_content">
+          <h1 className="center-text_3">{t("gallery")} </h1>
+          <hr className="center-text-line_3" />
+          <div className="home_galereya_images">
+            <div>
+              <img src={saroy} alt="" />
+              <img src={chorvoq} alt="" />
             </div>
-          {/* </FullpageSection>
+            <div>
+              <img src={haykal} alt="" />
+            </div>
+            <div>
+              <img src={samar} alt="" />
+              <img src={naman} alt="" />
+            </div>
+          </div>
+          <button>{t("see_all")}</button>
+        </div>
+      </div>
+      {/* </FullpageSection>
           <FullpageSection style={{ SectionStyle }}> */}
-            <div className="home_about">
-              <h1 className="center-text_4">{t("about_Us")} </h1>
-              <hr className="center-text-line_4" />
-              <div class="home_about-content">
-                <div class="about-image_1">
-                  <img src={about1} alt="" />
-                </div>
-                <div class="about-img">
-                  <img src={about2} alt="" />
-                  <img src={about3} alt="" />
-                </div>
-                <div class="about-text">
-                  <div>
-                    <p>{t("decent_info")}</p>
-                    <button>{t("learn_more")} </button>
-                  </div>
-                </div>
-              </div>
+      <div className="home_about">
+        <h1 className="center-text_4">{t("about_Us")} </h1>
+        <hr className="center-text-line_4" />
+        <div class="home_about-content">
+          <div class="about-image_1">
+            <img src={about1} alt="" />
+          </div>
+          <div class="about-img">
+            <img src={about2} alt="" />
+            <img src={about3} alt="" />
+          </div>
+          <div class="about-text">
+            <div>
+              <p>{t("decent_info")}</p>
+              <button>{t("learn_more")} </button>
             </div>
-          {/* </FullpageSection>
+          </div>
+        </div>
+      </div>
+      {/* </FullpageSection>
           <FullpageSection style={{ SectionStyle }}> */}
-            <div className="home_contact">
-              <h1 className="center-text_5">{t("our_contacts")} </h1>
-              <hr className="center-text-line_5" />
-              <div className="home_contact_content">
-                <div className="">
-                  <div className="flex">
-                    <span>
-                      <MapIcon />
-                    </span>
-                    <p>{t("location")}</p>
-                  </div>{" "}
-                  <br />
-                  <div className="flex">
-                    <span>
-                      <PhoneIcon />
-                    </span>
-                    <p>99890 580-25-05 </p>
-                  </div>
-                </div>
-                <div className="home-contect-items">
-                  <div className="flex">
-                    <span>
-                      <MessengeIcon />
-                    </span>
-                    <p>contact@TourismUmrah.com</p>
-                  </div>{" "}
-                  <br />
-                  <div className="flex">
-                    <span>
-                      <FileIcon />
-                    </span>
-                    <p>fax@TourismUmrah.com</p>
-                  </div>
-                </div>
-              </div>
-              <button>{t("send_request")} </button>
+      <div className="home_contact">
+        <h1 className="center-text_5">{t("our_contacts")} </h1>
+        <hr className="center-text-line_5" />
+        <div className="home_contact_content">
+          <div className="">
+            <div className="flex">
+              <span>
+                <MapIcon />
+              </span>
+              <p>{t("location")}</p>
+            </div>{" "}
+            <br />
+            <div className="flex">
+              <span>
+                <PhoneIcon />
+              </span>
+              <p>99890 580-25-05 </p>
             </div>
-            {/* <Foo  ter /> */}
-          {/* </FullpageSection>
+          </div>
+          <div className="home-contect-items">
+            <div className="flex">
+              <span>
+                <MessengeIcon />
+              </span>
+              <p>contact@TourismUmrah.com</p>
+            </div>{" "}
+            <br />
+            <div className="flex">
+              <span>
+                <FileIcon />
+              </span>
+              <p>fax@TourismUmrah.com</p>
+            </div>
+          </div>
+        </div>
+        <button>{t("send_request")} </button>
+      </div>
+      {/* <Foo  ter /> */}
+      {/* </FullpageSection>
         </FullPageSections>
       </Fullpage> */}
     </div>
